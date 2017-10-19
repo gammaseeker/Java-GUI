4f419165aebe06b9d9ddb11e9d72aec5e932ea10
Joey <jjiemjitpolchai9540@bths.edu>
2017-10-19 01:54:08 -0400

    Lemme peep cl0ck

---
+++ Java-GUI/src/clock/DisplayClock.java
@@ -0,0 +1,29 @@
+package clock;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class DisplayClock extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		ClockPane clock = new ClockPane();
+		String timeString = clock.getHour() + ": " + clock.getMinute() + ":" + clock.getSecond();
+		Label lblCurrentTime = new Label(timeString);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(clock);
+		pane.setBottom(lblCurrentTime);
+		BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+		
+		Scene scene = new Scene(pane, 250, 250);
+		primaryStage.setTitle("DisplayClock"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+
+}
